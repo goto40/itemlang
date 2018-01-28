5bbc7af33e51a6367d638acb18336643f131ec4d
goto40 <users.noreply.github.com>
2018-01-28 15:00:49 +0100

    allow list of files as input

--- itemlang/commands/console.py
+++ itemlang/commands/console.py
@@ -4,9 +4,9 @@ from itemlang.itemc.codegen import codegen
 def itemc():
     import argparse
     parser = argparse.ArgumentParser(description='generate code for the custom idl model.')
-    parser.add_argument('model_file', metavar='model_file', type=str,
-                        help='model filename')
-    parser.add_argument('--src-gen-folder', dest='srcgen', default="srcgen", type=str,
+    parser.add_argument('model_files', metavar='model_files', type=str, nargs='+',
+                        help='model filenames')
+    parser.add_argument('--src-gen-folder', dest='srcgen', default="src-gen", type=str,
                         help='folder where to generate the code')
     parser.add_argument('--generate-cpp', dest='generate_cpp', default=False,
                         action='store_true', help='generate C++ code')
@@ -16,8 +16,9 @@ def itemc():
                         action='store_true', help='generate python code (construct based)')
 
     args = parser.parse_args()
-    codegen(model_file=args.model_file, srcgen_folder=expanduser(args.srcgen),
-            generate_cpp=args.generate_cpp,
-            generate_python=args.generate_python,
-            generate_python_construct=args.generate_python_construct)
+    for model_file in args.model_files:
+        codegen(model_file=model_file, srcgen_folder=expanduser(args.srcgen),
+                generate_cpp=args.generate_cpp,
+                generate_python=args.generate_python,
+                generate_python_construct=args.generate_python_construct)
 
