539615921d1536c3241745a4a42e98b5b87693d2
goto40 <users.noreply.github.com>
2018-01-30 20:21:37 +0100

    added manual algo test

---
+++ system_tests/cpp/tests/simple_adder1_test.cpp
@@ -0,0 +1,26 @@
+#include "catch.hpp"
+#include "algos/simple_adder1.h"
+
+TEST_CASE( "simple_adder1 test (manual)", "[algo]" ) {
+
+    project::example::Simple a,b,c;
+    a.init(3);
+    b.init(3);
+
+    for (size_t k=0;k<a.n;k++) {
+        a.a_ui16[k] = (k+1)*10;
+        b.a_ui16[k] = (k+1)*100;
+    }
+
+    algos::simple_adder1(a,b,c);
+
+    REQUIRE( c.n == 3 );
+    REQUIRE( a.n == b.n );
+    REQUIRE( a.n == c.n );
+
+    for (size_t k=0;k<c.n;k++) {
+        REQUIRE( c.a_ui16[k] == (k+1)*10 + (k+1)*100);
+    }
+
+}
+
