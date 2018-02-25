50cf2ff620b23f958448cdd0ac496b04523ae34b
goto40 <users.noreply.github.com>
2018-02-25 21:59:56 +0100

    prepated test py/cpp

--- system_tests/pycpp_example/run_tests.sh
+++ system_tests/pycpp_example/run_tests.sh
@@ -2,8 +2,9 @@
 itemc --generate-python model/ImageData.item --src-gen-folder src-python || exit 1
 itemc --generate-cpp    model/ImageData.item --src-gen-folder src-cpp || exit 1
 # run tests
+rm -rf data
+mkdir data
 cd src-python
-#jupyter nbconvert --to notebook --execute AlgoTestDataProvider.ipynb || exit 1
 jupyter nbconvert --to html --execute AlgoTestDataProvider.ipynb || exit 1
 cd -
 
