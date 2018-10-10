a7f227683e7b4d9986e1db260203fc97705ea240
goto40 <pierre.bayerl@googlemail.com>
2018-10-10 23:19:16 +0200

    editor with highlighting

--- itemlang/edit.py
+++ itemlang/edit.py
@@ -4,65 +4,27 @@ WORK IN PROGRESS: 1%
 """
 
 from __future__ import unicode_literals
-import traceback
 import itemlang.metamodel as custom_idl_metamodel
 import codecs
 import tkinter as tk
-import sys
-from textx.model import ObjCrossRef, ReferenceResolver
-from textx import get_children_of_type
-from textx.scoping import Postponed
-import textx
-import textx.scoping.tools as st
 from arpeggio import Parser, Sequence, NoMatch, EOF, Terminal, NonTerminal
-from textx.exceptions import TextXSyntaxError, TextXSemanticError
-from textx.const import MULT_OPTIONAL, MULT_ONE, MULT_ONEORMORE, \
-    MULT_ZEROORMORE, RULE_ABSTRACT, RULE_MATCH, MULT_ASSIGN_ERROR, \
-    UNKNOWN_OBJ_ERROR
 
 
-def all_terminals(pt,parent=None, parent_list=None):
+def all_terminals_and_links(pt,parent=None, parent_list=None):
     if parent_list is None: parent_list=[]
     for r in pt:
         if isinstance(r,Terminal):
-            yield r,parent, parent_list
+            yield r,parent, parent_list, False
         else:
-            yield from all_terminals(r,r,[r]+parent_list)
-
-def all_model_elements(model_obj,metamodel=None,current_metaattr=None):
-    """
-    Depth-first model object processing.
-    """
-    if metamodel is None:
-        metamodel = model_obj._tx_metamodel
-    many = [MULT_ONEORMORE, MULT_ZEROORMORE]
-
-    # enter recursive visit of attributes only, if the class of the
-    # object being processed is a meta class of the current meta model
-    if model_obj.__class__.__name__ in metamodel:
-        yield model_obj, current_metaattr
-        current_metaclass_of_obj = metamodel[model_obj.__class__.__name__]
-        for metaattr in current_metaclass_of_obj._tx_attrs.values():
-            # If attribute is base type or containment reference go down
-            if metaattr.cont:
-                attr = getattr(model_obj, metaattr.name)
-                if attr:
-                    if metaattr.mult in many:
-                        for _, obj in enumerate(attr):
-                            if obj:
-                                yield from all_model_elements(obj, metamodel, metaattr)
-                    else:
-                        yield from all_model_elements(attr, metamodel, metaattr)
-            else:
-                attr = getattr(model_obj, metaattr.name)
-                if attr:
-                    if metaattr.mult in many:
-                        for _, obj in enumerate(attr):
-                            if obj:
-                                yield obj, metaattr
-                    else:
-                        yield attr, metaattr
-
+            # parent.rule/_tx_class/_tx_attrs/<name>/cont==False & ref=True
+            # later: rule/_attr_name=<name>
+            if parent is not None and hasattr(r.rule, "_attr_name"):
+                name = r.rule._attr_name
+                metaattr = parent.rule._tx_class._tx_attrs[name]
+                if metaattr.ref and not metaattr.cont:
+                    yield r, parent, parent_list, True
+                    return
+            yield from all_terminals_and_links(r,r,[r]+parent_list)
 
 
 def edit(model_file):
@@ -94,29 +56,29 @@ def edit(model_file):
     r2pos1 = lambda o : model._tx_parser.pos_to_linecol(o.position-1)
     r2pos2 = lambda o : model._tx_parser.pos_to_linecol(o.position_end-1)
 
-    for r ,p, pl in all_terminals(pt):
-        print("Terminal {}:{}:{}-{}.{}-{}.{}.".format(r,r.rule_name,r.name,
-                                                      *r2pos1(r), *r2pos2(r)))
+    for r ,p, pl, islink in all_terminals_and_links(pt):
+        #print("Terminal {}:{}:{}-{}.{}-{}.{}.".format(r,r.rule_name,r.name,
+        #                                              *r2pos1(r), *r2pos2(r)))
         tag='keyword'
         # TODO analyze pl stack --> find outer element with same posK
-        if p is not None and p.name.startswith('__asgn'):
+        if islink:
+            tag = 'link'
+        elif p is not None and p.name.startswith('__asgn'):
             if (p.rule._attr_name=='name'):
                 tag = 'name_value'
             else:
                 tag = 'value'
-            # how to identify links?
-            # maybe I need to decorate the parse tree with links to the model?
-            # or the meta model?
         T.tag_add(tag,
                   '{}.{}'.format(*r2pos1(r)),
                   '{}.{}'.format(*r2pos2(r)))
+
 
     T.tag_config('keyword', foreground='black',
                  font=(fontname, 12, 'bold') )
     T.tag_config('value', foreground='green',
                  font=(fontname, 12, 'normal') )
     T.tag_config('link', foreground='blue',
-                 font=(fontname, 12, 'normal'))
+                 font=(fontname, 12, 'bold'))
     T.tag_config('name_value', foreground='magenta',
                  font=(fontname, 12, 'normal') )
 
